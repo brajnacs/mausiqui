e7c46cd10da7481e19c09a9d755dbb7466205100
Braj Mohan <brajnacs@gmail.com>
2021-08-19 18:00:06 +0530

    Setup environment handling and refactor npm scripts

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
+import {config} from 'dotenv'
 import express from 'express';
 
+config();
+
 const app = express();
-const port = 3000;
+const port = parseInt(process.env.APP_PORT || '3000', 10);
 
 app.get('/', (req, res) => {
   res.send('Keep playing the music')
